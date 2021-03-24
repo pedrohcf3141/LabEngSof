f0d590bc36211215155bd0f08b995501681e1037
Pedro Henrique Fernandes <pedro.fernandes@znc.com.br>
2021-03-23 21:02:33 -0300

    Rotas iniciais mocadas criação do storage e requrements

--- receitaria1_61/receitaria1_61/__init__.py
+++ receitaria1_61/receitaria1_61/__init__.py
@@ -1,8 +1,16 @@
 __version__ = '0.1.0'
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.secret_key = "receitaria"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from receitaria1_61.controllers import urls
